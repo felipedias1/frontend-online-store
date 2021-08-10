5473cc6adfbfe3de69e7f4f2742ceab8bc911888
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-10 16:53:18 -0300

    Correçao de lint

--- src/components/ListProducts.jsx
+++ src/components/ListProducts.jsx
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 
 class ListProducts extends Component {
-
   render() {
     return (
       <section>
         <div className="card-container">
-
           <h4>Lista de Produtos</h4>
-
         </div>
       </section>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,7 +39,7 @@ class Home extends Component {
     const { results } = await getProductsFromCategoryAndQuery(category, inputSearch);
     this.updateListProducts(results);
   }
-  
+
   onChangeCategory({ target: { value } }) {
     this.setState({ category: value }, () => {
       this.handleSubmit();
