92d67fb43fdf7fa1f92f7a3defaf4e50f23f0aae
João Paulo <jp.viegas@hotmail.com>
2021-08-12 17:56:19 -0300

    requisito 11

--- src/pages/PageDetails.jsx
+++ src/pages/PageDetails.jsx
@@ -1,53 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Form from '..components/Form'
+import { getProductsFromCategoryAndQuery } from '../services/api';
+import Form from '../components/Form';
+
 class PageDetails extends React.Component {
   constructor(props) {
     super(props);
 
-    // Com a props recebida do constructor (match/history/location) pego a match->params e desconstruo para pegar o id
-    const { id } = props.match.params;
-
     this.state = {
-      product: [],
-      productID: id, // Guardo no state o id recebido pela props do constructor
+      item: '',
     };
 
     // Permite o uso das funções abaixo para serem utilizadas em toda a classe com o .this
-    this.getProductById = this.getProductById.bind(this);
+    this.searchId = this.searchId.bind(this);
     // this.testFun = this.testFun.bind(this);
   }
 
   componentDidMount() {
-    this.getProductById();
+    this.searchId();
   }
 
-  async getProductById() {
-    // Desconstruo o state para pegar o productID
-    const { productID } = this.state;
-    // A variável response irá guardar o fetch de procura pelo ID do produto
-    const response = await fetch(`https://api.mercadolibre.com/items/${productID}`);
-    const selectedProduct = await response.json();
-    this.setState({
-      product: selectedProduct,
-    });
+  async searchId() {
+    const { match: { params: { id, category } } } = this.props;
+    const { results } = await getProductsFromCategoryAndQuery(category);
+    const valor = results.find((result) => result.id === id);
+    this.setState({ item: valor });
   }
 
   render() {
+    // const { produto: { title, thumbnail} } = this.props;
+    // const { produto } = this.props;
+    // const categoryId = produto.category_id;
     // setCart recebido pela props está no App.js
     const { setCart } = this.props;
-    const { product } = this.state;
-    const { title, thumbnail, price, id, available_quantity: available } = product;
+    const { item: { id, title, thumbnail, price } } = this.state;
+    // let star = 3;
+    const star = this.item;
     return (
-      <div className="detail-container">
-        <p data-testid="product-detail-name">{title}</p>
+      <div>
+        {/* <button type="button" onClick={ this.searchId }>olá</button> */}
+        <h1 data-testid="product-detail-name">{title}</h1>
         <img src={ thumbnail } alt={ title } />
-        <p>{ `R$ ${price}`}</p>
-        <p>{ `Disponível: ${available}` }</p>
+        <p>
+          R$
+          {price}
+        </p>
+        <p>
+          {star}
+        </p>
         <button
-          // Ao acionar o evento onClick é passado à função setCart os parametros abaixo
-          onClick={ () => setCart({ id, title, price, quant: 1, thumbnail, available }) }
           type="button"
+          // Ao acionar o evento onClick é passado à função setCart os parametros abaixo
+          onClick={ () => setCart({ id, title, price, quant: 1, thumbnail }) }
           data-testid="product-detail-add-to-cart"
         >
           Adicionar ao carrinho
@@ -59,12 +63,7 @@ class PageDetails extends React.Component {
 }
 
 PageDetails.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      name: PropTypes.string,
-    }),
-  }),
+  match: PropTypes.object,
 }.isRequired;
 
 export default PageDetails;
-
