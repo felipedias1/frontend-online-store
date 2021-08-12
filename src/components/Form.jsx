489a6806c92f0f4b78bfec2fe07a5e26b3d495ef
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-12 19:42:55 -0300

    Testando processos localstorage requisito 11

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,30 +1,89 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const Form = () => (
-  <form action="userForm">
-    <label htmlFor="rating">
-      Nota (entre 1 e 5):
-      <input
-        type="number"
-        id="rating"
-        min="1"
-        max="5"
-      />
-    </label>
-    <label htmlFor="comment">
-      Avalie o produto:
-      <textarea
-        name="comment"
-        id="comment"
-        form="userForm"
-        cols="30"
-        rows="10"
-        maxLength="50"
-        placeholder="Deixe seu comentário..."
-        data-testid="product-detail-evaluation"
-      />
-    </label>
-  </form>
-);
+class Form extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      nota: 0,
+      comment: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.registerLocalStorage = this.registerLocalStorage.bind(this);
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  registerLocalStorage = (event) => {
+    const { id } = this.props;
+    event.preventDefault();
+    const { email, nota, comment } = this.state;
+    let avaliacoes = JSON.parse(localStorage.getItem(id) || '[]');
+
+    const newRegister = {
+      id,
+      register: {
+        email,
+        nota,
+        comment,
+      },
+    };
+
+    avaliacoes = [...avaliacoes, newRegister];
+    localStorage.setItem(id, JSON.stringify(avaliacoes));
+  }
+
+  render() {
+    return (
+      <form action="userForm" onSubmit={ this.registerLocalStorage }>
+        <label htmlFor="email">
+          <input
+            id="email"
+            type="email"
+            placeholder="E-mail"
+            name="email"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="rating">
+          Nota (entre 1 e 5):
+          <input
+            type="number"
+            id="rating"
+            min="1"
+            max="5"
+            name="nota"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="comment">
+          Avalie o produto:
+          <textarea
+            name="comment"
+            id="comment"
+            form="userForm"
+            cols="30"
+            rows="10"
+            maxLength="50"
+            placeholder="Deixe seu comentário..."
+            data-testid="product-detail-evaluation"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button type="submit">Enviar</button>
+      </form>
+    );
+  }
+}
+
+Form.propTypes = {
+  id: PropTypes.string.isRequired,
+};
 
 export default Form;
